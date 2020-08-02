28b6b684cc66ae705fbf1732d1690d25f797112a
GwendolyneFreddy <gwendolyne.freddy@gmail.com>
2020-08-02 08:20:26 +0200

    Reformat dialogues

--- darron/d/darron.d
+++ darron/d/darron.d
@@ -1,31 +1,28 @@
 BEGIN ~DARRON~
 
-IF ~NumTimesTalkedTo(0)~  THEN BEGIN 1
-      SAY @0
-      IF ~~ THEN REPLY @1 GOTO 4
-      IF ~~ THEN REPLY @2 GOTO 3
-      IF ~~ THEN REPLY @3 DO ~StartStore("ESLISTO0",LastTalkedToBy())~ EXIT
-      IF ~~ THEN REPLY @4 EXIT   
-   END
+IF ~NumTimesTalkedTo(0)~ THEN BEGIN 1
+	SAY @0
+	IF ~~ THEN REPLY @1 GOTO 4
+	IF ~~ THEN REPLY @2 GOTO 3
+	IF ~~ THEN REPLY @3 DO ~StartStore("eslisto0",LastTalkedToBy())~ EXIT
+	IF ~~ THEN REPLY @4 EXIT
+END
 
 IF ~!NumTimesTalkedTo(0)~ THEN BEGIN 2
-      SAY @5
-      IF ~~ THEN REPLY @3 DO ~StartStore("ESLISTO0",LastTalkedToBy())~ EXIT
-      IF ~~ THEN REPLY @2 GOTO 3
-      IF ~~ THEN REPLY @4 EXIT   
-   END
+	SAY @5
+	IF ~~ THEN REPLY @3 DO ~StartStore("eslisto0",LastTalkedToBy())~ EXIT
+	IF ~~ THEN REPLY @2 GOTO 3
+	IF ~~ THEN REPLY @4 EXIT
+END
 
 IF ~~ THEN BEGIN 3
-      SAY @6  
-      IF ~~ THEN REPLY @7 DO ~StartStore("ESLISTO0",LastTalkedToBy())~ EXIT
-      IF ~~ THEN REPLY @4 EXIT
-   END
+	SAY @6
+	IF ~~ THEN REPLY @7 DO ~StartStore("eslisto0",LastTalkedToBy())~ EXIT
+	IF ~~ THEN REPLY @4 EXIT
+END
 
 IF ~~ THEN BEGIN 4
-      SAY @8
-=
-          @9      
-      IF ~~ THEN REPLY @7 DO ~StartStore("ESLISTO0",LastTalkedToBy())~ EXIT
-      IF ~~ THEN REPLY @4 EXIT
-   END
-
+	SAY @8 = @9
+	IF ~~ THEN REPLY @7 DO ~StartStore("eslisto0",LastTalkedToBy())~ EXIT
+	IF ~~ THEN REPLY @4 EXIT
+END
